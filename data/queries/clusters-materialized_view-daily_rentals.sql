fe2ee639015f1a34a20e27259dbdfb06f13f807f
baggiponte <lucabaggi.uni@gmail.com>
2021-11-15 17:10:11 +0100

    naive forecaster added

--- data/queries/clusters-materialized_view-daily_rentals.sql
+++ data/queries/clusters-materialized_view-daily_rentals.sql
@@ -2,7 +2,7 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS clusters_daily_rentals AS
 (
 SELECT drb2019.data_partenza,
        bcs.cluster_nil || ' - ' || bcs.cluster AS cluster,
-       SUM(drb2019.noleggi_giornalieri) AS noleggi_giornalieri
+       SUM(drb2019.noleggi_giornalieri)        AS noleggi_giornalieri
 FROM daily_rentals_before_2019 drb2019
          JOIN bikemi_clustered_stalls bcs on drb2019.numero_stazione = bcs.numero_stazione
 GROUP BY drb2019.data_partenza, bcs.cluster, bcs.cluster_nil
